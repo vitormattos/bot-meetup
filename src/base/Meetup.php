128920c43b0b658795c49baa0ecf23b16e94146f
Vitor Mattos <vitor.mattos@phprio.org>
2017-01-14 21:03:22 -0200

    Implementado método para chamada de endpoints da api de meetup

--- src/base/Meetup.php
+++ src/base/Meetup.php
@@ -3,9 +3,10 @@ namespace Base;
 
 class Meetup extends \League\OAuth2\Client\Provider\GenericProvider
 {
+    private $token;
     public static function getProvider()
     {
-        return new \League\OAuth2\Client\Provider\GenericProvider([
+        return new self([
             'clientId'                => getenv('CLIENT_ID'),
             'clientSecret'            => getenv('CLIENT_SECRET'),
             'redirectUri'             => getenv('REDIRECT_URI'),
@@ -13,5 +14,28 @@ class Meetup extends \League\OAuth2\Client\Provider\GenericProvider
             'urlAccessToken'          => 'https://secure.meetup.com/oauth2/access',
             'urlResourceOwnerDetails' => 'https://api.meetup.com/2/member/self'
         ]);
+    }
+    
+    public function setToken($token)
+    {
+        $this->token = $token;
+    }
+    
+    public function getCommand($method, $path, $args = null)
+    {
+        $factory = $this->getRequestFactory();
+        $request = $factory->getRequest($method, 'https://api.meetup.com'.$path,
+            [
+                'Authorization' => 'Bearer '.$this->token,
+                'Content-Type'=>'application/x-www-form-urlencoded'
+            ],
+            $args
+        );
+        $response = $this->sendRequest($request);
+        $parsed = $this->parseResponse($response);
+
+        $this->checkResponse($response, $parsed);
+
+        return $parsed;
     }
 }
